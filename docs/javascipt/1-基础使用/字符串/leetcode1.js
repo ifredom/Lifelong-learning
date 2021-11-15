7468d6582386fb2dd110ae93db303e3262f026ba
youxspace computer <1950735817@qq.com>
2021-11-15 18:14:06 +0800

    两数之和

---
+++ docs/javascipt/1-基础使用/字符串/leetcode1.js
@@ -0,0 +1,15 @@
+// 1. 两数之和
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target == nums[i] + nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+var nums = [2, 5, 5, 11];
+var target = 10;
+var pos = twoSum(nums, target);
+console.log(pos);
