7a22aaa9438251c6eaba2e737f957a00190e2b3e
ifredom <ifredomvip@gmail.com>
2022-03-25 21:56:56 +0800

    fix: update

--- docs/刷题/35.搜索插入位置.js
+++ docs/刷题/35.搜索插入位置.js
@@ -2,24 +2,24 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
+ *
+ * 奇数 中间  偶数取小
  */
 var searchInsert = function (nums, target) {
-  var left = 0;
-  var right = nums.length - 1;
-
-  while (left < right) {
-    var mid = (left + right) >> 1;
-    if (nums[mid] == target) {
-      return nums[mid];
-    } else if (nums[mid] < target) {
-      left = mid + 1;
-    } else if (nums[mid] > target) {
+  var left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    var mid = ((right - left) >> 1) + left;
+    console.log(mid);
+    if (target <= nums[mid]) {
       right = mid - 1;
+    } else {
+      left = mid + 1;
     }
   }
 
-  return nums[left] >= target ? left : left + 1;
+  return right + 1;
 };
 
-var result = searchInsert([1, 3, 5, 6, 9, 13, 18, 23], 7);
+var result = searchInsert([1, 3, 5, 6], 5);
 console.log("result:", result);
